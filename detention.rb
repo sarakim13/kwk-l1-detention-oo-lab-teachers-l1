8481c2fef35f520b57e57ab51f71b14335ebdb5a
Sara Kim <sarakim0903@gmail.com>
2018-06-22 14:09:44 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,50 @@
 #Code your detention class here
+class Detention 
+  class Detention
+
+  def initialize
+    @activity = "collective punishment"
+    @students = []
+  end
+
+  def students
+    @students
+  end
+
+  def remove_student(name)
+    @students.delete(name)
+  end
+
+  def add_student(name)
+    @students << name
+  end
+
+  def location=(location)
+    @location = location
+  end
+
+  def location
+    @location
+  end
+
+  def time=(time)
+    @time = time
+  end
+
+  def time
+    @time
+  end
+
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+
+
+  def activity
+    @activity
+  end
+end
